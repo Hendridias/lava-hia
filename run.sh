b40ad9fdc7ecc6068640f561202554034e24b9c8
Hendri wowo <164765944+Hendridias@users.noreply.github.com>
2024-03-24 22:02:08 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9a3d42d5-dacc-4d5d-9ea7-1df9041fc651/)
 
 
 echo "$curl_output" >> Update.tsx
